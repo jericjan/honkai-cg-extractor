b0827fde9655125e6b22c45d7a7d266ae57f47d6
jericjan <cantilfrederick@gmail.com>
2022-09-13 19:22:05 +0800

    support for manual file-path arg in command-line

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import shutil
 import os
 from shlex import join as shlexjoin
 import glob
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('files', metavar='FILES', type=str, nargs='*', default='none',
+                    help='.usm files to be converted')
+args = vars(parser.parse_args())
+
 
 def run_sub(coms):    
     with Popen(coms, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True) as proc:
@@ -13,17 +20,6 @@ def run_sub(coms):
 def get_full_path(rel_path):
     return os.path.join(os.getcwd(),rel_path)
 
-if not os.path.exists("CGs/"):
-    os.mkdir("CGs")
-
-ofw_path = get_full_path("OpenedFilesView\\OpenedFilesView.exe")
-run_sub([ofw_path, "/scomma", "opened_files.txt", "/wildcardfilter", "*.usm"])
-with open("opened_files.txt",encoding="utf-8") as f:
-    opened_files = f.read()
-opened_files = opened_files.split("\n")    
-opened_files = [x for x in opened_files if x ]
-print(f"{len(opened_files)} CGs found")
-
 def ask_audio():    
     while True:
         with_audio = input("Do you want to include audio? (y/n)\n").lower()
@@ -32,34 +28,59 @@ def ask_audio():
         elif with_audio == "n":
             return False
         else:
-            print("Try again")            
+            print("Try again")  
             
-     
-if opened_files:
-    with_audio = ask_audio()
-    file_paths = [file.split(",")[1] for file in opened_files]    
-    for file in file_paths:    
-        dest_path = os.path.join(os.getcwd(),"CGs\\")
-        shutil.copy(file,dest_path)
-        usm_file = os.path.join(dest_path,os.path.basename(file))
-        just_name = os.path.splitext(usm_file)[0]
-        print(f"Demuxing {os.path.basename(file)}")
-        demuxer_path = get_full_path("UsmDemuxer/UsmDemuxer.exe")
-        run_sub([demuxer_path,usm_file])
-        os.remove(usm_file)        
-        vid_file = glob.glob(f"{just_name}*.m2v")[0]
-        aud_file = glob.glob(f"{just_name}*.hca")[0]
-        mkv_file = f"{just_name}.mkv"
-        mp4_file = f"{just_name}.mp4"
-        if with_audio:            
-            run_sub(["ffmpeg", "-i", vid_file, "-i", aud_file, "-map","0:v:0","-map","1:a:0","-y",mkv_file]) 
-        else:            
-            run_sub(["ffmpeg", "-i", vid_file, "-c","copy","-y",mp4_file])   
-        os.remove(vid_file)
-        os.remove(aud_file)
-        os.remove("opened_files.txt")
-        print("Done!")
-        print(f'Check your "{dest_path}" folder for the CGs')
-        input()
-else:
-    print("There are no opened .usm files. Exiting...")
+def main():            
+    if not os.path.exists("CGs/"):
+        os.mkdir("CGs")
+    if args['files'] != "none":        
+        opened_files = args['files']
+        print(f"{len(opened_files)} CGs provided")
+    else:
+        ofw_path = get_full_path("OpenedFilesView\\OpenedFilesView.exe")
+        run_sub([ofw_path, "/scomma", "opened_files.txt", "/wildcardfilter", "*.usm"])
+        with open("opened_files.txt",encoding="utf-8") as f:
+            opened_files = f.read()
+        opened_files = opened_files.split("\n")    
+        opened_files = [x for x in opened_files if x ]
+        opened_files = [file.split(",")[1] for file in opened_files]    
+        print(f"{len(opened_files)} CGs found")
+                              
+    if opened_files:
+        with_audio = ask_audio()
+        file_paths = opened_files
+        for file in file_paths:    
+            dest_path = os.path.join(os.getcwd(),"CGs\\")
+            shutil.copy(file,dest_path)
+            usm_file = os.path.join(dest_path,os.path.basename(file))
+            just_name = os.path.splitext(usm_file)[0]
+            print(f"Demuxing {os.path.basename(file)}")
+            demuxer_path = get_full_path("UsmDemuxer/UsmDemuxer.exe")
+            run_sub([demuxer_path,usm_file])
+            os.remove(usm_file)        
+            vid_file = glob.glob(f"{just_name}*.m2v")[0]
+            audio_file_exists = True
+            try:
+                aud_file = glob.glob(f"{just_name}*.hca")[0]
+            except IndexError:
+                print("No audio file found.")
+                audio_file_exists = False
+            mkv_file = f"{just_name}.mkv"
+            mp4_file = f"{just_name}.mp4"
+            if with_audio:            
+                run_sub(["ffmpeg", "-i", vid_file, "-i", aud_file, "-map","0:v:0","-map","1:a:0","-y",mkv_file]) 
+            else:            
+                run_sub(["ffmpeg", "-i", vid_file, "-c","copy","-y",mp4_file])   
+            os.remove(vid_file)
+            if audio_file_exists:
+                os.remove(aud_file)
+            if os.path.exists("opened_files.txt"):
+                os.remove("opened_files.txt")
+            print("Done!")
+            print(f'Check your "{dest_path}" folder for the CGs')
+            input()
+    else:
+        print("There are no opened .usm files. Exiting...")
+
+if __name__ == "__main__":
+    main()
